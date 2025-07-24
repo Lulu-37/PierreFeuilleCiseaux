fba6564bd04a8998b0e7c67eaa47a2d794b4dc39
Lulu-37 <lucillerichard37@yahoo.fr>
2025-07-24 15:44:41 +0200

    étape 2 validée

---
+++ script.js
@@ -0,0 +1,9 @@
+var tab = ["Pierre" , "Feuille" , "Ciseaux"]
+
+function getComputerChoice () {
+    const indexAleatoire = Math.floor (Math.random () * 3)
+    let choose = tab[indexAleatoire]
+    
+    console.log (choose)
+}
+getComputerChoice()
