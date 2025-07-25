ccaa5a52e78706f772d01ee3b0f2224d92169f96
Lulu-37 <lucillerichard37@yahoo.fr>
2025-07-25 14:58:48 +0200

    choix humain 1

--- script.js
+++ script.js
@@ -1,13 +1,3 @@
-var tab = ["pierre" , "feuille" , "ciseaux"]
-
-function getComputerChoice() {
-    const indexAleatoire = Math.floor (Math.random () * 3)
-    let choose = tab[indexAleatoire]
-    
-    console.log (choose)
-}
-getComputerChoice()
-
 
 function getHumanChoice() {
     let person = prompt("Choississez pierre, feuille ou ciseaux");
@@ -22,6 +12,19 @@ var computerScore = 0
 
 
 
+var tab = ["pierre" , "feuille" , "ciseaux"]
+
+function getComputerChoice() {
+    const indexAleatoire = Math.floor (Math.random () * 3)
+    let choose = tab[indexAleatoire]
+    
+    console.log (choose)
+}
+getComputerChoice()
+
+
+
+
 function playRound(humanChoice, computerChoice) {
   // your code here!
 }
@@ -30,3 +33,5 @@ const humanSelection = getHumanChoice();
 const computerSelection = getComputerChoice();
 
 playRound(humanSelection, computerSelection);
+
+
